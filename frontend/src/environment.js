156a540775232b5f2a4b23439fc151c42d34065c
channa88 <124447711+channa88@users.noreply.github.com>
2025-08-10 01:12:54 +0530

    Update environment.js

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,9 @@
 let IS_PROD = true;
 
-// The server URL will be read from an environment variable during deployment.
-// When you run 'npm start' locally, it will default to localhost.
-const server = process.env.https://connectlive-bhhq.onrender.com || 'srv-d2au3ofdiees73e6cpu0';
+const server = IS_PROD ?
+    "https://apnacollegebackend.onrender.com" :
+
+    "http://localhost:8000"
+
 
 export default server;
